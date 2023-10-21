67073e7eb543d92b38b0b6352f2b7a3a9068d1ee
Leonardo dos Santos Dias <dias.leonardodossantos@gmail.com>
2023-10-21 20:27:09 -0300

    feat: add categories on catalog page

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,5 +1,28 @@
-const CatalogPage = () => {
-  return <div>Catalog</div>;
+import { Badge } from "@/components/ui/badge";
+import { ShapesIcon } from "lucide-react";
+import CategoryItem from "./components/category-item";
+import { prismaClient } from "@/lib/prisma";
+
+const CatalogPage = async () => {
+  const categories = await prismaClient.category.findMany();
+
+  return (
+    <div className="p-5">
+      <Badge
+        className="uppercase gap-1 rounded-full text-base py-[0.375rem] border-primary border-2 px-3"
+        variant={"outline"}
+      >
+        <ShapesIcon size={16} />
+        Catálogo
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-8">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CatalogPage;
