b68ff66c8a0be28dd24fb990da93fa9a3a38163e
Leonardo dos Santos Dias <dias.leonardodossantos@gmail.com>
2023-10-21 15:52:05 -0300

    feat: add categories

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,23 @@
 "use client";
 
 import Image from "next/image";
+import Categories from "./components/categories";
 
 export default function Home() {
   return (
     <div className="p-5">
-      <Image 
-        src={"/banner-home01.png"} 
+      <Image
+        src={"/banner-home01.png"}
         height={0}
         width={0}
         alt="até 55% de desconto esse mês"
         className="h-auto w-full"
         sizes="100vw"
-        >
-        </Image>
+      ></Image>
+
+      <div className="mt-8">
+        <Categories />
+      </div>
     </div>
   );
 }
