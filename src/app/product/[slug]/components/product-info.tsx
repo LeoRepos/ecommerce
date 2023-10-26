eb77e2079f8c10c9336af593b036066449f15776
Leonardo dos Santos Dias <dias.leonardodossantos@gmail.com>
2023-10-26 20:39:09 -0300

    feat: add productInfo components

---
+++ src/app/product/[slug]/components/product-info.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { ProductWithTotalPrice } from "@/helpers/product";
+import {
+  ArrowDownIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  TruckIcon,
+} from "lucide-react";
+import { useState } from "react";
+
+interface ProductIndoProps {
+  product: Pick<
+    ProductWithTotalPrice,
+    "name" | "basePrice" | "description" | "discountPercentage" | "totalPrice"
+  >;
+}
+
+const ProductInfo = ({
+  product: { name, basePrice, totalPrice, description, discountPercentage },
+}: ProductIndoProps) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleClickIncrease = () => {
+    setQuantity((prev) => prev + 1);
+  };
+
+  const handleClickDecrese = () => {
+    setQuantity((prev) => (prev === 1 ? prev : prev - 1));
+  };
+
+  return (
+    <div className="flex flex-col px-5">
+      <h2 className="text-lg">{name}</h2>
+
+      <div className="flex items-center gap-8">
+        <h1 className="text-xl font-bold">R$ {totalPrice.toFixed(2)}</h1>
+        {discountPercentage > 0 && (
+          <Badge className="px-2 py-[2px]">
+            <ArrowDownIcon size={14} /> {discountPercentage}
+          </Badge>
+        )}
+      </div>
+
+      {discountPercentage > 0 && (
+        <p className="text-sm line-through opacity-75">
+          R$ {Number(basePrice).toFixed(2)}
+        </p>
+      )}
+
+      <div className="flex items-center gap-2 mt-4">
+        <Button size="icon" variant="outline" onClick={handleClickDecrese}>
+          <ArrowLeftIcon size={16} />
+        </Button>
+        <span>{quantity}</span>
+        <Button size="icon" variant="outline" onClick={handleClickIncrease}>
+          <ArrowRightIcon size={16} />
+        </Button>
+      </div>
+
+      <div className="flex flex-col gap-3 mt-8">
+        <h3 className="font-bold">Descrição</h3>
+        <p className="text-sm opacity-60 text-justify">{description}</p>
+      </div>
+
+      <Button className="mt-8 uppercase font-bold">
+        Adicionar ao carrinho
+      </Button>
+
+      <div className="bg-accent flex items-center mt-4 rounded-lg px-6 py-2 justify-between">
+
+        <div className="flex justify-center items-center gap-4">
+            <div className="">
+            <TruckIcon />
+            </div>
+
+            <div className="flex flex-col">
+                <div className="text-xs">
+                    Entrega via <span className="font-bold italic">FSPacket®</span>
+                </div>
+                <div className="text-primary text-xs">
+                    Envio para <span className="font-bold">todo Brasil</span>
+                </div>
+            </div>
+        </div>
+
+        <div className="font-bold">
+            Frete Grátis
+        </div>
+      </div>
+
+      <h2 className="uppercase text-base font-bold mt-16">Produtos recomendados</h2>
+
+    </div>
+  );
+};
+
+export default ProductInfo;
